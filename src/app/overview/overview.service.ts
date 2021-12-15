490c3b89497856b1e3ad66ea976d7da243e60776
IRSHATH <mdirshath12@gmail.com>
2021-12-15 18:32:21 +0530

    Added redirects file and env

--- src/app/overview/overview.service.ts
+++ src/app/overview/overview.service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { salesCount, salesCounts } from './overview.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OverviewService {
+  baseUrl = environment.baseUrl
 
   constructor(private http:HttpClient) { }
 
   Salescount(name:any): Observable<salesCounts> {
-    return this.http.post<salesCounts>(`http://localhost:3000/superdash_app/api/v1/shopify/orders/ordersCount`, name );
+    return this.http.post<salesCounts>(`${this.baseUrl}superdash_app/api/v1/shopify/orders/ordersCount`, name );
   }
   
 }
