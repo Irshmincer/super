22dac8d56b352cbc322e06ca2ff0d47f68aa6356
IRSHATH <mdirshath12@gmail.com>
2021-12-21 18:55:50 +0530

    Added baseurl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: `http://ec2-3-109-206-226.ap-south-1.compute.amazonaws.com:3001/`
+  baseUrl: `https://simmanager.10kflutter.com/`
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true, 
-  baseUrl: `http://ec2-3-109-206-226.ap-south-1.compute.amazonaws.com:3001/`
+  baseUrl: `https://simmanager.10kflutter.com/`
 };
 
 /*
